ee174800d3e597d1cd7067b6062d67af279d8941
Cristian Perez <cristianshawn82@gmail.com>
2023-08-07 21:07:36 -0600

    fusion menuMobile y Carrito

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 const menuEmail = document.querySelector('.navbar-email');
+const burgerIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const carritoMenu = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+    // const isCarritoMenuCloset = carritoMenu.classList.contains('inactive');
 
-    // if (desktopMenu.style.display === 'none') {
-    //     desktopMenu.style.display = 'block';
-    // } else {
-    //     desktopMenu.style.display = 'none'
+    // if (!isCarritoMenuCloset) {
+    //     carritoMenu.classList.add('inactive');
     // }
+
+    carritoMenu.classList.add('inactive');
+
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    // const isCarritoMenuCloset = carritoMenu.classList.contains('inactive');
+
+    // if (!isCarritoMenuCloset) {
+    //     carritoMenu.classList.add('inactive');
+    // }
+
+    carritoMenu.classList.add('inactive');
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoMenu() {
+    // const isMobileMenuCloset = mobileMenu.classList.contains('inactive');
+
+    // if (!isMobileMenuCloset) {
+    //     mobileMenu.classList.add('inactive');
+    // }
+    
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
+    carritoMenu.classList.toggle('inactive');
 }
